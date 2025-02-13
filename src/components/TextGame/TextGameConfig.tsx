92891f5ca24be04f58e64f45c780591725c05efd
Jon Ginty <j.ginty@outlook.com>
2025-02-13 11:09:05 +0000

    add app title and sound effect test

--- src/components/TextGame/TextGameConfig.tsx
+++ src/components/TextGame/TextGameConfig.tsx
@@ -1,6 +1,7 @@
-import { Fieldset, Input, Select, Slider, Stack, Switch } from "@mantine/core";
+import { Button, Fieldset, Group, Input, Select, Slider, Stack, Switch } from "@mantine/core";
 import TextGameSettings from "./TextGame.settings";
 import NoteRange from "../NoteRange/NoteRange";
+import { synthesizeSequence } from "../../engine/audio/synthesize";
 
 
 const TextGameConfig = ({ settings, handleChange }: TextGameConfigProps) => {
@@ -12,13 +13,26 @@ const TextGameConfig = ({ settings, handleChange }: TextGameConfigProps) => {
             handleChange({ ...settings, ...val })
         }
     }
+
+    const testSfx = async () => {
+        try {
+            await synthesizeSequence(["C4", "E4", "G4", "C5"], 100);
+        } catch (e: unknown) {
+            alert("Error playing sound: " + e);
+        }
+    }
     
     return (
         <Fieldset m="xs" legend="Settings">
             <Stack>
                 <Select label="Scale" data={["C major", "A minor"]} value={settings.scale} onChange={makeChange("scale")}></Select>
                 <Input.Wrapper label={"Notes per round: " + settings.noteCount}><Slider value={settings.noteCount} max={10} min={1} onChange={makeChange("noteCount")}></Slider></Input.Wrapper>
-                <Input.Wrapper label="Sound effects (may be loud!)"> <Switch checked={settings.sfx} onChange={() => handleChange({...settings, sfx: !settings.sfx})} ></Switch></Input.Wrapper>
+                <Input.Wrapper label="Sound effects (may be loud!)">
+                    <Group>
+                        <Switch checked={settings.sfx} onChange={() => handleChange({...settings, sfx: !settings.sfx})} ></Switch>
+                        <Button variant="subtle" onClick={() => testSfx()}>Test</Button>
+                    </Group>
+                </Input.Wrapper>
                 <Input.Wrapper label={`Note range: (${settings.lowestNote} - ${settings.highestNote})`}><NoteRange lowest={settings.lowestNote} highest={settings.highestNote} lowChange={makeChange("lowestNote")} highChange={makeChange("highestNote")}></NoteRange></Input.Wrapper>
                 <Input.Wrapper label={"Max interval between notes: " + settings.maxInterval}><Slider value={settings.maxInterval} max={100} min={1} onChange={makeChange("maxInterval")}></Slider></Input.Wrapper>
             </Stack>
