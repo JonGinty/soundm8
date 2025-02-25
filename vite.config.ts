540fefe51431d1429fbe8e5edb9985906c0cc194
Jon Ginty <j.ginty@outlook.com>
2025-02-25 17:04:25 +0000

    Use manual chunking to keep bundle size under control

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,31 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      filename: './dist/stats.html',
+      open: true, // Automatically open the visualization in the browser
+    }),
+  ],
   base: '/soundm8/',
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/testSetup.ts',
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', '@mantine/core'],
+          vexflow: ['vexflow'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Adjust the limit as needed
   },
 })
